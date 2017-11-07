c7b12b99c0b32a4dcbef019d46adac13c3350def
Shane Ursani <shaneursani@gmail.com>
2017-11-06 20:26:03 -0600

    rename namespace

--- ui-services/Controllers/ValuesController.cs
+++ ui-services/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ui_services.Controllers
+namespace Titan.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
--- ui-services/Startup.cs
+++ ui-services/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace ui_services
+namespace Titan
 {
     public class Startup
     {
