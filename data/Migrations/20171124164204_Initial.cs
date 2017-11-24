03d55aaa22f2ad307a48233a01e0fae3a54998e0
Shane Ursani <shaneursani@gmail.com>
2017-11-24 13:26:09 -0600

    postgres migrations with flyway

---
+++ data/Migrations/20171124164204_Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int4", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    DateOfBirth = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    FirstName = table.Column<string>(type: "text", nullable: true),
+                    Gender = table.Column<int>(type: "int4", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: true),
+                    UserName = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
---
+++ data/Migrations/Scripts/V1__initial.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    "MigrationId" varchar(150) NOT NULL,
+    "ProductVersion" varchar(32) NOT NULL,
+    CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
+);
+
+CREATE TABLE "User" (
+    "Id" serial NOT NULL,
+    "DateOfBirth" timestamp NOT NULL,
+    "Email" text NULL,
+    "FirstName" text NULL,
+    "Gender" int4 NOT NULL,
+    "LastName" text NULL,
+    "UserName" text NULL,
+    CONSTRAINT "PK_User" PRIMARY KEY ("Id")
+);
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20171124164204_Initial', '2.0.0-rtm-26452');
+
